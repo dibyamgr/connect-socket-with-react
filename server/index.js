804102180a29d5bb747e3e2a8bc5341b44f4d0f0
dibyamgr <dibyamagar5@gmail.com>
2022-08-25 21:23:40 +0545

    Socket IO instantiation on Server side and receiving events from client

--- server/index.js
+++ server/index.js
@@ -13,11 +13,29 @@ const server = http.createServer(app); // this is how we create an http server w
 //variable that we will use to do anything with socket in backend 
 // as Server is a class, we instantiate the variable (new instance of it) with new keyword
 // we pass our http server
+// some information related to cors
 const io = new Server(server, {
-    // some information related to cors
     cors: {
         origin: "http://localhost:3000", //url origin of where our Frontend will be
         methods: ["GET", "POST"] //what methods we will be using
     }
-})  
+});
 
+// listen to some events from FE
+// on - first arg-> connection event, second arg - function starts running
+// whenever a user is connected, 
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`)
+    socket.on("send_message", (data) => {
+        // receive or listen to data sent by Frontend
+        console.log(data);
+        // broadcast to everyone who is connected to this socket server
+        // sent event to FE
+        socket.broadcast.emit("receive_message", data)
+    })
+})
+
+// set it to listen to a port
+server.listen(3001, () => [
+    console.log("SERVER is running")
+])
